8f6866582bae305a758b8058cbeea4a4bbd274db
“Anthony <anthony.deguire619@gmail.com>
2024-02-12 15:49:50 -0800

    update

---
+++ blackjack.js
@@ -0,0 +1,19 @@
+var dealerTotal = 0;
+var playerTotal = 0;
+
+var dealerAceTotal = 0;
+var playerAceTotal = 0;
+
+var backOfCard;
+var deck;
+
+var hit = true;
+
+window.onload = function() {
+  createDeck();
+}
+
+function createDeck() {
+  let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let suits = ["C", "D", "H", "S"];
+}
