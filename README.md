aa82f44e51edf4537cdba27f73d018e12d1acd57
“Anthony <anthony.deguire619@gmail.com>
2024-02-18 10:14:12 -0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Milestone-Project
+
+Hello, my name is Anthony DeGuire and welcome to my milestone-project, 21, also known as blackjack.
+
+There is not any inspiration behind the game, but I enjoy playing blackjack with friends and family at local casinos.
+
+The objective of the game is to get 21 or as close as possible. Number cards keep their value, face cards (jack, queen, king, queen) are worth 10 and Aces are worth either 1 or 11
--- blackjack.js
+++ blackjack.js
@@ -1,5 +1,6 @@
-var dealerTotal = 0;
-var playerTotal = 0;
+// listing variables 
+var dealerTotal = 0; // total count for dealer
+var playerTotal = 0; // total count for player
 
 var dealerAceTotal = 0;
 var playerAceTotal = 0;
@@ -11,9 +12,71 @@ var hit = true;
 
 window.onload = function() {
   createDeck();
+  shuffleDeck();
+  startGame();
+
 }
 
-function createDeck() {
+function createDeck() { // creating the deck 
   let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   let suits = ["C", "D", "H", "S"];
+  deck = [];
+
+  for (let x  = 0; x < suits.length; x++) {
+    for (let y = 0; y < values.length; y++) {
+      deck.push(values[y] + "-" + suits[x]); // loops through each suits first, and for each suit we loop through all the values
+    }
+  }
+  console.log(deck);
+}
+/*********************************************************
+ * Title: black-jack
+ * Author: ImKennyYip
+ * Year: 2022
+ * Code version: Programming code
+ * Availability: https://github.com/ImKennyYip/black-jack
+ *********************************************************/
+function shuffleDeck() {
+  for (let x = 0; x < deck.length; x++) {
+    let y = Math.floor(Math.random() * deck.length); 
+    let temp = deck[x];
+    deck[x] = deck[y];
+    deck[y] = temp;
+  }
+  console.log(deck);
+}
+
+function startGame() {
+  backOfCard = deck.pop(); // removes the last card of the array
+  dealerTotal += getValue(backOfCard);
+  dealerAceTotal += checkAce(backOfCard)
+  console.log(hidden);
+}
+
+function getValue(card) {
+  let data = card.split("-"); // Example: "10-S" would be ["10", "S"]
+  let value = data[0];
+
+  /*********************************************************
+ * Title: black-jack
+ * Author: ImKennyYip
+ * Year: 2022
+ * Code version: Programming code
+ * Availability: https://github.com/ImKennyYip/black-jack
+ *********************************************************/
+  if (isNaN(value)) {
+    if (value == "A") {
+        return 11;
+    }
+    return 10;
+  }
+
+  return parseInt(value); // parseInt() method parses a string and returns first integer
+}
+
+function checkAce(card) {
+  if (card[0] == "A") {
+    return 1;
+  }
+  return 0;
 }
